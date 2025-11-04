a4ceb1a2ca07aac30c3ff134fed1f2e3f9b5360d
Seyi Jegede <seyijegede23@gmail.com>
2025-11-04 22:32:37 +0100

    First commit

---
+++ relianticonandico/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className='flex justify-between items-center px-4 sm:12
+    lg:px-24 xl:-px-40 sticky top-0 z-20
+     background-blur-xl font-medium bg-white/50 
+     dark:bg-gray-900/70'>
+        <img src=''/>
+        </div>
+  )
+}
+
+export default navbar
